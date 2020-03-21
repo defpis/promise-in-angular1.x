3e918660f242b28b144bbd87fe1e3ea7552e12ac
defpis <wenjun@datagrand.com>
2020-03-21 21:23:17 +0800

    docs(*): add more comment

--- src/promise.ts
+++ src/promise.ts
@@ -5,6 +5,7 @@ function processQueue(state: any): void {
     const deferred = handlers[0];
     const fn = handlers[state.status];
     try {
+      // 只要是函数直接resolve其返回值
       if (_.isFunction(fn)) {
         deferred.resolve(fn(state.value));
       } else if (state.status === 1) {
@@ -13,18 +14,22 @@ function processQueue(state: any): void {
         deferred.reject(state.value);
       }
     } catch (e) {
+      // 任何异常触发reject
       deferred.reject(e);
     }
   });
+  // 清空回调队列
   state.pending = [];
 }
 
 function scheduleProcessQueue(state: any): void {
+  // angular1.x中使用$rootScope.$evalAsync延迟
   setTimeout(() => {
     processQueue(state);
   }, 0);
 }
 
+// 根据resolved状态，包裹value为一个promise对象
 function makePromise(value: any, resolved: boolean): Promise {
   const d = new Deferred();
   if (resolved) {
@@ -41,6 +46,8 @@ function handleFinallyCallback(
   resolved: boolean,
 ): Promise {
   const callbackValue = callback();
+  // 如果回调是一个promise，等待这个promise值有效后再触发后续链式
+  // 在finally中调用，总是返回原始值
   if (callbackValue && callbackValue.then) {
     return callbackValue.then(function() {
       return makePromise(value, resolved);
@@ -55,8 +62,11 @@ export class Promise {
 
   constructor() {
     this.$$state = {
+      // 初始值
       value: undefined,
+      // 初始状态
       status: 0,
+      // 回调队列
       pending: [],
     };
   }
@@ -66,8 +76,14 @@ export class Promise {
     onRejected: any = null,
     onProgress: any = null,
   ): any {
+    // 构建另一个Deferred在异步回调中resolve或reject触发链式回调
     const result = new Deferred();
     this.$$state.pending.push([result, onFulfilled, onRejected, onProgress]);
+    // 确保这种情况下依然能够执行回调 以及链式的时候能够正常运行
+    // d.resolve(1)
+    // setTimeout(() => {
+    //   d.promise.then((v) => console.log(v))
+    // }, 10);
     if (this.$$state.status > 0) {
       scheduleProcessQueue(this.$$state);
     }
@@ -100,6 +116,8 @@ export class Deferred {
     }
 
     if (value && _.isFunction(value.then)) {
+      // 如果值是一个promise，把当前deferred方法挂载到其回调
+      // 也就是说value的状态直接决定当前deferred.promise的状态
       value.then(
         _.bind(this.resolve, this),
         _.bind(this.reject, this),
@@ -141,16 +159,17 @@ export class Deferred {
   }
 }
 
-const $Q = function Q(resolver: any): Promise {
+// 实现类似ES6风格的Promise
+function Q(resolver: any): Promise {
   if (!_.isFunction(resolver)) {
     throw 'Expected function, got ' + resolver;
   }
   const d = new Deferred();
   resolver(_.bind(d.resolve, d), _.bind(d.reject, d));
   return d.promise;
-};
+}
 
-export default _.extend($Q, {
+export default _.extend(Q, {
   defer(): Deferred {
     return new Deferred();
   },
@@ -183,19 +202,23 @@ export default _.extend($Q, {
     const d = this.defer();
     _.forEach(promises, (promise: any, index: any) => {
       counter++;
+      // 包裹一层立刻有效的promise
       this.when(promise).then(
         (value: any) => {
           results[index] = value;
           counter--;
+          // 当所有promise计算完成时，resolve整个结果
           if (!counter) {
             d.resolve(results);
           }
         },
+        // 任意一个出错导致reject
         (value: any) => {
           d.reject(value);
         },
       );
     });
+    // 空对象或空数组
     if (!counter) {
       d.resolve(results);
     }
